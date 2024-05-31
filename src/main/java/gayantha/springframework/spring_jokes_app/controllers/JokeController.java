20238498db0498cad76c7f724d889ded08963da9
GayanthaSam <galuthgama@gmail.com>
2024-06-01 03:24:57 +0900

    Added view. minor fix in serviceImpl. closes #5

--- src/main/java/gayantha/springframework/spring_jokes_app/controllers/JokeController.java
+++ src/main/java/gayantha/springframework/spring_jokes_app/controllers/JokeController.java
@@ -1,5 +1,6 @@
 package gayantha.springframework.spring_jokes_app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ public class JokeController {
 
     private final JokeService jokeService;
 
+    @Autowired
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
