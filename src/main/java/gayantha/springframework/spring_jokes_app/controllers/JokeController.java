6466ff6ade487cfc4e3595475d25b41431dfc928
GayanthaSam <galuthgama@gmail.com>
2024-06-01 03:06:11 +0900

    adding controller. Closes #4

---
+++ src/main/java/gayantha/springframework/spring_jokes_app/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package gayantha.springframework.spring_jokes_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import gayantha.springframework.spring_jokes_app.sevices.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({ "/", "" })
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
